b091f27867c1e16efb3667538fec48ae952efb23
Enes <eneskurbetoglu1@gmail.com>
2022-11-13 15:06:34 +0300

    fix

--- saveme.py
+++ saveme.py
@@ -7,12 +7,11 @@ import base64
 
 from bs4 import BeautifulSoup
 
-from config import API_KEY
-
 lockfile_path = ""
 gamedir = ""
 host, port = "https://127.0.0.1", ""
 lobby_endpoint = "lol-champ-select/v1/session"
+summoner_endpoint = "lol-summoner/v1/summoners"
 password = ""
 headers = {}
 url = ""
@@ -20,6 +19,9 @@ stats_url = "https://lolprofile.net/summoner/"
 region = ""
 
 class GameNotActiveException(Exception):
+    pass
+
+class ClientIsNotAlive(Exception):
     pass
 
 def main(args):
@@ -36,19 +38,24 @@ def main(args):
     except FileNotFoundError:
         print("Please run the script with the game directory as argument!")
         return
-    with open(lockfile_path, "rb") as f:
-        tmp = f.readline().decode().split(":")
-        port = tmp[2]
-        password = tmp[3]
-        b64 = base64.b64encode(bytes(f"riot:{password}", "utf-8"))
-        headers = {
-            "Authorization": f"Basic {str(b64.decode())}",
-            "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
-        }
+    try:
+        with open(lockfile_path, "rb") as f:
+            tmp = f.readline().decode().split(":")
+            port = tmp[2]
+            password = tmp[3]
+            b64 = base64.b64encode(bytes(f"riot:{password}", "utf-8"))
+            headers = {
+                "Authorization": f"Basic {str(b64.decode())}",
+                "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
+            }
+    except FileNotFoundError:
+        raise ClientIsNotAlive("Client has not been launched or the game directory is wrong!")
     url = f"{host}:{port}/{lobby_endpoint}"
     r = requests.get(url, headers=headers, verify="./riotgames.pem")
     res = r.json()
-    player_ids = [player["puuid"] for player in res.get("myTeam", None)]
+    if res.get("myTeam", None) is None:
+        raise GameNotActiveException("There is no active game!")
+    player_ids = [player["summonerId"] for player in res.get("myTeam", None)]
     sumNames = []
     with open(gamedir + "\\Config\\LeagueClientSettings.yaml", "rb") as f:
         lines = f.readlines()
@@ -58,19 +65,22 @@ def main(args):
                 region = tmp[""]
                 stats_url += region.lower() +"/"
     for player_id in player_ids:
-        res = requests.get(f"https://{region}1.api.riotgames.com/lol/summoner/v1/summoners/by-puuid/{player_id}?api_key={API_KEY}")
-        sumNames.append(res.json()["name"])
+        url = f"{host}:{port}/{summoner_endpoint}/{player_id}"
+        res = requests.get(url, headers=headers, verify="./riotgames.pem")
+        sumNames.append(res.json()["displayName"])
     winRates = []
     for sumName in sumNames:
-        r = requests.get(stats_url + sumName)
-        page = r.content
-        soup = BeautifulSoup(page.decode("utf-8"), "html5lib")
-        wrate = soup.find_all("div", "tooltip block s-wrb")[0]
-        wrate = wrate.find("div")
-        wrate = str(wrate)[5:-7]
-        winRates.append(float(wrate))
-    if len(winRates) == 0:
-        raise GameNotActiveException("There is no active game!")
+        try:
+            r = requests.get(stats_url + sumName)
+            page = r.content
+            soup = BeautifulSoup(page.decode("utf-8"), "html5lib")
+            wrate = soup.find_all("div", "tooltip block s-wrb")[0]
+            wrate = wrate.find("div")
+            wrate = str(wrate)[5:-7]
+            winRates.append(float(wrate))
+        except Exception:
+            print(f"Summoner profile could not be accessed for summoner: {sumName}!")
+            continue
     print("Likely to win!" if (sum(winRates)/len(winRates)) > 50 else "Likely to lose!")
     print("Average win rate:", sum(winRates)/len(winRates))
 
