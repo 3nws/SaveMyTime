ec171306129ae57e3411a62e846bce8c9c23133d
Enes <eneskurbetoglu1@gmail.com>
2022-11-13 17:51:41 +0300

    add auto accept

--- README.md
+++ README.md
@@ -11,4 +11,8 @@ DIR: Game directory. Only required the first time.
 
 Will tell you if you are likely to win the current league of legends game.
 
-Uses LCU API to fetch data from your game client and scrapes winrates from [LoLProfile](https://lolprofile.net).
+Uses [LCU API](http://www.mingweisamuel.com/lcu-schema/tool) to fetch data from your game client and scrapes winrates from [LoLProfile](https://lolprofile.net).
+
+## TODO
+
+- Account for dodges.
--- saveme.py
+++ saveme.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
-import os
+import time
 import sys
 import base64
 
@@ -12,20 +12,16 @@ gamedir = ""
 host, port = "https://127.0.0.1", ""
 lobby_endpoint = "lol-champ-select/v1/session"
 summoner_endpoint = "lol-summoner/v1/summoners"
+ready_check_endpoint = "lol-matchmaking/v1/ready-check/accept"
 password = ""
 headers = {}
 url = ""
 stats_url = "https://lolprofile.net/summoner/"
 region = ""
-
-class GameNotActiveException(Exception):
-    pass
-
-class ClientIsNotAlive(Exception):
-    pass
+lobby_found = False
 
 def main(args):
-    global stats_url
+    global stats_url, port, host, url, headers, region, lobby_endpoint, summoner_endpoint, ready_check_endpoint, gamedir
     if len(args) > 0:
         with open("./path.txt", "w+") as f:
             f.write(args[0])
@@ -49,14 +45,8 @@ def main(args):
                 "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"
             }
     except FileNotFoundError:
-        raise ClientIsNotAlive("Client has not been launched or the game directory is wrong!")
-    url = f"{host}:{port}/{lobby_endpoint}"
-    r = requests.get(url, headers=headers, verify="./riotgames.pem")
-    res = r.json()
-    if res.get("myTeam", None) is None:
-        raise GameNotActiveException("There is no active game!")
-    player_ids = [player["summonerId"] for player in res.get("myTeam", None)]
-    sumNames = []
+        print("Client has not been launched or the game directory is wrong!")
+        return
     with open(gamedir + "\\Config\\LeagueClientSettings.yaml", "rb") as f:
         lines = f.readlines()
         for line in lines:
@@ -64,6 +54,16 @@ def main(args):
                 tmp = eval("{"+found+"}")
                 region = tmp[""]
                 stats_url += region.lower() +"/"
+
+def listen_to_champ_select():
+    global stats_url, port, host, url, headers, region, lobby_endpoint, summoner_endpoint, ready_check_endpoint, gamedir, lobby_found
+    url = f"{host}:{port}/{lobby_endpoint}"
+    r = requests.get(url, headers=headers, verify="./riotgames.pem")
+    res = r.json()
+    if res.get("myTeam", None) is None:
+        return
+    player_ids = [player["summonerId"] for player in res.get("myTeam", None)]
+    sumNames = []
     for player_id in player_ids:
         url = f"{host}:{port}/{summoner_endpoint}/{player_id}"
         res = requests.get(url, headers=headers, verify="./riotgames.pem")
@@ -83,7 +83,15 @@ def main(args):
             continue
     print("Likely to win!" if (sum(winRates)/len(winRates)) > 50 else "Likely to lose!")
     print("Average win rate:", sum(winRates)/len(winRates))
+    lobby_found = True
 
+def listen_to_ready_check():
+    requests.post(f"{host}:{port}/{ready_check_endpoint}", headers=headers, verify="./riotgames.pem")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+    while True:
+        if not lobby_found:
+            listen_to_champ_select()
+        listen_to_ready_check()
+        time.sleep(.5)
